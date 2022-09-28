37bc4210d766b16b2f9a1d017fade80e19d74353
Mohamed ElGemeie <mgalal2002@outlook.com>
2022-09-28 18:16:29 +0200

    Api key for firebase
    create the api key and package.json

---
+++ api_key.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+  apiKey: “AIzaSyAIK_3KhZn0vZKCV35ASHd3VjawXSuS3r0",
+  authDomain: “radicalx-6759d.firebaseapp.com”,
+  projectId: “radicalx-6759d”,
+  storageBucket: “radicalx-6759d.appspot.com”,
+  messagingSenderId: “454144471628",
+  appId: “1:454144471628:web:1e1b59a04b4c121c7fbae9”
+};
+
+// Package.json
+{
+  "type": "service_account",
+  "project_id": "radicalx-6759d",
+  "private_key_id": "6aa967888022d70fa025ec5b73fa49b0bc37fbec",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDM/bcTbHbR1Z1G\nA1t8VYOeDuiC+T9U1E5j+I1KE2Gp+25UR0QnSBGHUJl9LTGvPcJHDp4lcqk6nHnJ\nK4r61ZPeT08FWTnsPlpm+cwc69aoQ8gk5CXt1bYV7/INADxbVeMj67n6V/h9Luft\nNyBsSRyJkR4H3vxe2UJhMFMpz0NF14TuLq2qUpckcZqUmjPqIu7zXPYsTaoYH1/K\nIgQNPGsTZdCMa6RB5R+jEX2Wp4be2Z00gCd2NIwGw0rjC5XI0hQWJIDeHMKjGrMs\nHBbtlRag6PsFZlaVZZ7JFFBbBWNry2nVWYipAs8GfC3tiTiYftKgfiRCOU0suYZX\nxFPcOJSPAgMBAAECggEAMvmQkhLxxSJSlrt0/4kgTqi0ZwVmG0kkKTYApIAHVEbC\n29BRAVepjDWdxYP8i/BWpGbdKsDFll6c5kqep9t+htS/+LuEbNvgjSTm/fYjjfo1\nBf958xemzPO7BEKfv7Ku5hrTR46iqzaD0/hWZrcTkp30ks5/gXKTUCm9NHNm7jHE\nuNwYxVnSuDXmylYA5BQu8pxPXAlPePOigs4Vmu4Qtu8zUP5FX+RbzVZAgbW4T/zY\nOrPqUoxyVqX0RMc7OIuTub0rnQYIkPvG9zdtuuBN1L9PRIVv+3BWGdHu7kytM5Pt\nEovKUtnx5TxoNx0/q8ZFVvxfntMUCkMchC2NjUJu6QKBgQDwx2wYEQbdFsMMba8v\ngKspXlwtbENgU5m1DdcY+PfFvkHG7v83lQ8FGyeJc5HlGhKEIJzGIa5hlwxrdm6f\n/7wSmWDRU1prRD1U65+gzos6dzLqotIDv7D+mJKoET7+fM7RGd4eoFjpgI3xNVCD\nnjt8PHIY5Gr6egjleKyhp/zn8wKBgQDZ8yEflxZZNea+h7ty10C2oYC+0SbelcWv\n6ZIWLs4JhVXkYglYhMi5kVKG6sQq8GnJXVal6+jhE5x/59Z39vEmTYvqUeJwpSeL\nSbtz9uMktB2lIXT6NbnhLDewvFOu/qVfj+CwOYM5+AAXLxx3cocN/JiP9UFnrdlx\nPS9TaB5D9QKBgDpWp0shfP8+j53ISaUB0PbiBQWAcMa7GsfyhYZodGg2E+vmZnIK\nLKx5CthT5yqSaaAX4sRizsSYe4riFCWyKdh7zyiIwKSy0QVtXazxvmw5C14seyQr\nYvAc+ZO9Mf0pMlcZpGbZRRG2cy3ZVBu/z/vnDWIYng74WIV4+qLkd/OfAoGBAJ3F\n5QqLQwBdhqYSd92QeSY8D4kSuwKShULs4HXyd/Bod18WoRFVPkEnNAi61jsHQm0k\nhl8iQbj9i2MEHztEphlGNazwje1hgRjRzUMLTC77vCIX8ChY0DBy4/WqvyGE8Y4W\nB19KovZJ0zb1QbQuUhoL6xz7O9ekAC57TlEgpBgpAoGAZjI5DhWvaTevxBlNHhbb\nXo+WUhj70Tn1JY/NGPKE6FPHKlk8f+a7mgJNunvtobUvBH9l6JnYvKPqSLkVbKsk\nuXH0rtrgImJp/xQzYBnIwvAlYxffJ1GS4tQZCfcbzNK/pETnEozOL8F+i9FTsOjD\n/KRexnz9NuSy/IStmYKJxig=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-ekzje@radicalx-6759d.iam.gserviceaccount.com",
+  "client_id": "115554305098683504505",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ekzje%40radicalx-6759d.iam.gserviceaccount.com"
+}
